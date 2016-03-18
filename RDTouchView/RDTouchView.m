21de0c95644899dd813ee1ec3fce70866c4959f4
Katagiri <katagiri@plala.co.jp>
2016-03-18 15:04:07 +0900

    add License

--- RDTouchView/RDTouchView.m
+++ RDTouchView/RDTouchView.m
@@ -3,7 +3,9 @@
 //  RDTouchView
 //
 //  Created by 片桐奏羽 on 2016/03/18.
-//  Copyright © 2016年 SoKatagiri. All rights reserved.
+//  Copyright © 2016年 SoKatagiri. 
+//  This software is released under the MIT License.
+//   http://opensource.org/licenses/mit-license.php
 //
 
 #import "RDTouchView.h"
